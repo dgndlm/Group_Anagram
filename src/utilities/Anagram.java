43c638ab6ed57b7029e5fd61b07d8551f7177fe9
aliaksandrpetrukhnov <petrukhnou@gmail.com>
2023-05-02 19:46:34 -0400

    Changed Anagram.java

--- src/utilities/Anagram.java
+++ src/utilities/Anagram.java
@@ -2,11 +2,14 @@ package utilities;
 
 public class Anagram {
 
+
     public static void main(String[] args) {
 
         String name = " Alex";
         int age;
         String address;
 
-    }
-}
+        public static void main (String[]args){
+            System.out.println(" Hello");
+        }
+    }}
