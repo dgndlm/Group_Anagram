5b827eee7ec6094cedd8dc3ceb0a114646c331e3
aliaksandrpetrukhnov <petrukhnou@gmail.com>
2023-05-02 19:38:12 -0400

    Add Anagram.java

--- src/utilities/Anagram.java
+++ src/utilities/Anagram.java
@@ -1,11 +1,12 @@
 package utilities;
 
 public class Anagram {
-    public static void main(String[] args){
-        String str1="Git Practice";
-        int numberOfPeople=4;
 
-     System.out.println("Hello");
+    public static void main(String[] args) {
+
+        String name = " Alex";
+        int age;
+        String address;
 
     }
 }
